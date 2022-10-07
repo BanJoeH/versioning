4b69b89514db9f4b0d0c3e135569fa57f9edf05a
DianomiJH <joe.harrison@dianomi.com>
2022-10-07 16:10:37 +0100

    trying here"

---
+++ helpers/git.js
@@ -0,0 +1,160 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const { GITHUB_REPOSITORY, ENV } = process.env;
+
+module.exports = new (class Git {
+  commandsRun = [];
+
+  constructor() {
+    const githubToken = core.getInput('github-token');
+
+    // Make the Github token secret
+    core.setSecret(githubToken);
+
+    const gitUserName = core.getInput('git-user-name');
+    const gitUserEmail = core.getInput('git-user-email');
+    const gitUrl = core.getInput('git-url');
+
+    // if the env is dont-use-git then we mock exec as we are testing a workflow
+    if (ENV === 'dont-use-git') {
+      this.exec = (command) => {
+        const fullCommand = `git ${command}`;
+
+        // eslint-disable-next-line no-console
+        console.log(`Skipping "${fullCommand}" because of test env`);
+
+        if (!fullCommand.includes('git remote set-url origin')) {
+          this.commandsRun.push(fullCommand);
+        }
+      };
+    }
+
+    // Set config
+    this.config('user.name', gitUserName);
+    this.config('user.email', gitUserEmail);
+
+    // Update the origin
+    if (githubToken) {
+      this.updateOrigin(`https://x-access-token:${githubToken}@${gitUrl}/${GITHUB_REPOSITORY}.git`);
+    }
+  }
+
+  /**
+   * Executes the git command
+   *
+   * @param command
+   * @return {Promise<>}
+   */
+  // eslint-disable-next-line class-methods-use-this, no-async-promise-executor
+  exec = (command) =>
+    // eslint-disable-next-line no-async-promise-executor
+    new Promise(async (resolve, reject) => {
+      let execOutput = '';
+
+      const options = {
+        listeners: {
+          stdout: (data) => {
+            execOutput += data.toString();
+          },
+        },
+      };
+
+      const exitCode = await exec.exec(`git ${command}`, null, options);
+
+      if (exitCode === 0) {
+        resolve(execOutput);
+      } else {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject(`Command "git ${command}" exited with code ${exitCode}.`);
+      }
+    });
+
+  /**
+   * Set a git config prop
+   *
+   * @param prop
+   * @param value
+   * @return {Promise<>}
+   */
+  config = (prop, value) => this.exec(`config ${prop} "${value}"`);
+
+  /**
+   * Add a file to commit
+   *
+   * @param file
+   * @returns {*}
+   */
+  add = (file) => this.exec(`add ${file}`);
+
+  /**
+   * Commit all changes
+   *
+   * @param message
+   *
+   * @return {Promise<>}
+   */
+  commit = (message) => this.exec(`commit -m "${message}"`);
+
+  /**
+   * Pull the full history
+   *
+   * @return {Promise<>}
+   */
+  pull = async () => {
+    const args = ['pull'];
+
+    // Check if the repo is unshallow
+    if (await this.isShallow()) {
+      args.push('--unshallow');
+    }
+
+    args.push('--tags');
+    args.push(core.getInput('git-pull-method'));
+
+    return this.exec(args.join(' '));
+  };
+
+  /**
+   * Push all changes
+   *
+   * @return {Promise<>}
+   */
+  push = (branch) => this.exec(`push origin ${branch} --follow-tags`);
+
+  /**
+   * Check if the repo is shallow
+   *
+   * @return {Promise<>}
+   */
+  isShallow = async () => {
+    if (ENV === 'dont-use-git') {
+      return false;
+    }
+
+    const isShallow = await this.exec('rev-parse --is-shallow-repository');
+
+    return isShallow.trim().replace('\n', '') === 'true';
+  };
+
+  /**
+   * Updates the origin remote
+   *
+   * @param repo
+   * @return {Promise<>}
+   */
+  updateOrigin = (repo) => this.exec(`remote set-url origin ${repo}`);
+
+  /**
+   * Creates git tag
+   *
+   * @param tag
+   * @return {Promise<>}
+   */
+  createTag = (tag) => this.exec(`tag -a ${tag} -m "${tag}"`);
+
+  /**
+   * Validates the commands run
+   */
+})();
--- index.js
+++ index.js
@@ -1,113 +1,210 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
 const core = require('@actions/core');
-const github = require('@actions/github');
 const shell = require('shelljs');
+const fs = require('fs');
+const conventionalRecommendedBump = require('conventional-recommended-bump');
+const conventionalChangelogAngular = require('conventional-changelog-angular');
+const git = require('./helpers/git');
 
-try {
-  if (!shell.which('git')) {
-    shell.echo('\nSorry, this script requires git');
-    shell.exit(1);
-  }
+async function run() {
+  const gitBranch = core.getInput('git-branch').replace('refs/heads/', '');
+conventionalChangelogAngular()
+  conventionalRecommendedBump(
+    {
+      preset: 'angular',
+    },
+    async (err, reccomendation) => {
+      if (err) {
+        console.log('error reccommending bump', err);
+        shell.exit(1);
+      }
+      console.log('err : ', err);
+      console.log('releaseType : ', JSON.stringify(reccomendation));
+      const { releaseType } = reccomendation;
+      const releaseTypeMap = {
+        major: 'major',
+        minor: 'minor',
+        patch: 'patch',
+      };
+      const releaseTypeLabel = releaseTypeMap[releaseType];
+      const releaseTypeLabelCapitalized =
+        releaseTypeLabel.charAt(0).toUpperCase() + releaseTypeLabel.slice(1);
+      const releaseTypeLabelEmoji = {
+        major: '🚨',
+        minor: '✨',
+        patch: '🐛',
+      };
+      const releaseTypeEmoji = releaseTypeLabelEmoji[releaseType];
 
-  shell.echo('\nChecking commit messages for `feat: `');
+      const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+      const OLD_VERSION = packageJson.version;
+      let [major, minor, patch] = OLD_VERSION.split('.');
+      const releaseMessage = `chore: ${releaseTypeEmoji} ${releaseTypeLabelCapitalized} release`;
+      switch (releaseType) {
+        case 'major':
+          major = parseInt(major, 10) + 1;
+          minor = 0;
+          patch = 0;
+          break;
+        case 'minor':
+          minor = parseInt(minor, 10) + 1;
+          patch = 0;
+          break;
+        case 'patch':
+          patch = parseInt(patch, 10) + 1;
+          break;
+        default:
+          break;
+      }
+      const NEW_VERSION = [major, minor, patch].join('.');
 
-  const currentBranch = shell
-    .exec('git rev-parse --abbrev-ref HEAD', { silent: true })
-    .stdout.trim();
-  shell.echo(`\nCurrent branch: ${currentBranch}`);
-  if (currentBranch === 'main') {
-    shell.echo(
-      '\nYou are on the main branch. Please switch to a feature branch before running this script.'
-    );
-    shell.exit(1);
-  }
+      shell.echo(`\nOld version: ${OLD_VERSION}`);
+      shell.echo(`New version: ${NEW_VERSION}`);
 
-  const commitMessages = shell.exec(`git log --oneline main...${currentBranch}`, {
-    silent: true,
-  }).stdout;
+      // prepend version to release notes
+      const releaseNotes = fs.readFileSync('./src/release.md', 'utf8');
 
-  const hasFeatureCommit = commitMessages.split('\n').some((commit) => commit.includes('feat: '));
+      if (releaseNotes.startsWith(`#### v`)) {
+        const releaseNotesArray = releaseNotes.split('\n').filter(Boolean);
+        const releaseNotesVersion = releaseNotesArray[0].replace('#### v', '');
+        shell.echo('\nRelease notes already have version at start');
+        if (releaseNotesVersion.toString() === OLD_VERSION.toString()) {
+          shell.echo('\nRelease notes version is equal to old version. Exiting.');
+          shell.exit(1);
+        }
 
-  const hasReleaseNotesChanged = shell
-    .exec('git diff --name-only main...HEAD', { silent: true })
-    .stdout.includes('release.md');
+        if (releaseNotesVersion.toString() !== NEW_VERSION.toString()) {
+          shell.echo('\nRelease notes version is not equal to new version. Exiting.');
+          shell.echo(
+            '\nEither updated release notes version to match new version \nor remove version from release notes and this script will automagically add the correct one.'
+          );
+          shell.exit(1);
+        }
+      }
 
-  if (!hasFeatureCommit && !hasReleaseNotesChanged) {
-    shell.echo('\nNo feature commits or release notes changes found. Exiting.');
-    shell.exit(0);
-  }
+      try {
+        const copyPackageJson = { ...packageJson };
+        copyPackageJson.version = NEW_VERSION;
+        fs.writeFileSync('package.json', `${JSON.stringify(copyPackageJson, null, 2)}\n`);
 
-  if (hasFeatureCommit && !hasReleaseNotesChanged) {
-    shell.echo('\nFeature commits found but no release notes changes found. Exiting.');
-    shell.exit(1);
-  }
-  if (hasReleaseNotesChanged && !hasFeatureCommit) {
-    shell.echo('\nRelease notes changes found but no feature commits found. Exiting.');
-    shell.exit(1);
-  }
+        shell.echo('\nPackage.json version updated');
 
-  shell.echo('\nFeature commits and release notes changes found. Proceeding.');
+        const newReleaseNotes = `#### v${NEW_VERSION}\n${releaseNotes}`;
+        fs.writeFileSync('./src/release.md', newReleaseNotes);
 
-  const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-  const OLD_VERSION = packageJson.version;
-  const [major, minor, patch] = OLD_VERSION.split('.');
-  const NEW_VERSION = `${major}.${parseInt(minor, 10) + 1}.${patch}`;
+        shell.echo('\nRelease notes updated version updated');
+        await git.add('.');
+        await git.commit(releaseMessage);
+        await git.createTag(`v${NEW_VERSION}`);
+        await git.push(gitBranch);
+      } catch (e) {
+        shell.echo(e);
+        try {
+          // try to revert changes if there was an error
+          fs.writeFileSync('package.json', `${JSON.stringify(packageJson, null, 2)}\n`);
+          shell.echo('\nPackage.json version reverted');
+          fs.writeFileSync('./src/release.md', releaseNotes);
+          shell.echo('\nRelease notes version reverted');
+          shell.exit(1);
+        } catch (error) {
+          // if there was an error reverting changes, exit
+          shell.echo(`\nError reverting package.json and release notes changes: ${error}`);
+          shell.echo(error);
+          shell.exit(1);
+        }
+      }
+    }
+  );
 
-  console.log(OLD_VERSION);
-  console.log(NEW_VERSION);
+  // shell.echo(`\nCommit messages:\n${commitMessages}`);
+
+  // const hasFeatureCommit = commitMessages.split('\n').some((commit) => commit.includes('feat: '));
+
+  // const hasReleaseNotesChanged = shell
+  //   .exec('git diff --name-only main...HEAD', { silent: true })
+  //   .stdout.includes('release.md');
+
+  // if (!hasFeatureCommit && !hasReleaseNotesChanged) {
+  //   shell.echo('\nNo feature commits or release notes changes found. Exiting.');
+  //   shell.exit(0);
+  // }
+
+  // if (hasFeatureCommit && !hasReleaseNotesChanged) {
+  //   shell.echo('\nFeature commits found but no release notes changes found. Exiting.');
+  //   shell.exit(1);
+  // }
+  // if (hasReleaseNotesChanged && !hasFeatureCommit) {
+  //   shell.echo('\nRelease notes changes found but no feature commits found. Exiting.');
+  //   shell.exit(1);
+  // }
+
+  // shell.echo('\nFeature commits and release notes changes found. Proceeding.');
+
+  // const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+  // const OLD_VERSION = packageJson.version;
+  // const [major, minor, patch] = OLD_VERSION.split('.');
+  // const NEW_VERSION = `${major}.${parseInt(minor, 10) + 1}.${patch}`;
+
+  // shell.echo(`\nOld version: ${OLD_VERSION}`);
+  // shell.echo(`New version: ${NEW_VERSION}`);
 
   // prepend version to release notes
-  const releaseNotes = fs.readFileSync('./src/release.md', 'utf8');
+  // const releaseNotes = fs.readFileSync('./src/release.md', 'utf8');
 
-  if (releaseNotes.startsWith(`#### v`)) {
-    const releaseNotesArray = releaseNotes.split('\n').filter(Boolean);
-    const releaseNotesVersion = releaseNotesArray[0].replace('#### v', '');
-    shell.echo('\nRelease notes already have version at start');
-    if (releaseNotesVersion.toString() === OLD_VERSION.toString()) {
-      shell.echo('\nRelease notes version is equal to old version. Exiting.');
-      shell.exit(1);
-    }
+  // if (releaseNotes.startsWith(`#### v`)) {
+  //   const releaseNotesArray = releaseNotes.split('\n').filter(Boolean);
+  //   const releaseNotesVersion = releaseNotesArray[0].replace('#### v', '');
+  //   shell.echo('\nRelease notes already have version at start');
+  //   if (releaseNotesVersion.toString() === OLD_VERSION.toString()) {
+  //     shell.echo('\nRelease notes version is equal to old version. Exiting.');
+  //     shell.exit(1);
+  //   }
 
-    if (releaseNotesVersion.toString() !== NEW_VERSION.toString()) {
-      shell.echo('\nRelease notes version is not equal to new version. Exiting.');
-      shell.echo(
-        '\nEither updated release notes version to match new version \nor remove version from release notes and this script will automagically add the correct one.'
-      );
-      shell.exit(1);
-    }
-  }
+  //   if (releaseNotesVersion.toString() !== NEW_VERSION.toString()) {
+  //     shell.echo('\nRelease notes version is not equal to new version. Exiting.');
+  //     shell.echo(
+  //       '\nEither updated release notes version to match new version \nor remove version from release notes and this script will automagically add the correct one.'
+  //     );
+  //     shell.exit(1);
+  //   }
+  // }
 
-  try {
-    const copyPackageJson = { ...packageJson };
-    copyPackageJson.version = NEW_VERSION;
-    fs.writeFileSync('package.json', `${JSON.stringify(copyPackageJson, null, 2)}\n`);
+  // try {
+  //   const copyPackageJson = { ...packageJson };
+  //   copyPackageJson.version = NEW_VERSION;
+  //   fs.writeFileSync('package.json', `${JSON.stringify(copyPackageJson, null, 2)}\n`);
 
-    shell.echo('\nPackage.json version updated');
+  //   shell.echo('\nPackage.json version updated');
 
-    const newReleaseNotes = `#### v${NEW_VERSION}\n${releaseNotes}`;
-    fs.writeFileSync('./src/release.md', newReleaseNotes);
+  //   const newReleaseNotes = `#### v${NEW_VERSION}\n${releaseNotes}`;
+  //   fs.writeFileSync('./src/release.md', newReleaseNotes);
 
-    shell.echo('\nRelease notes updated version updated');
+  //   shell.echo('\nRelease notes updated version updated');
 
-    if (shell.exec('git commit -am "chore: update version"').code !== 0) {
-      shell.echo('\nError committing changes. Exiting.');
-      throw new Error('Error committing changes');
-    }
-  } catch (e) {
-    shell.echo(e);
-    try {
-      // try to revert changes if there was an error
-      fs.writeFileSync('package.json', `${JSON.stringify(packageJson, null, 2)}\n`);
-      shell.echo('\nPackage.json version reverted');
-      fs.writeFileSync('./src/release.md', releaseNotes);
-      shell.echo('\nRelease notes version reverted');
-      shell.exit(1);
-    } catch (error) {
-      // if there was an error reverting changes, exit
-      shell.echo(`\nError reverting package.json and release notes changes: ${error}`);
-      shell.echo(error);
-      shell.exit(1);
-    }
-  }
+  //   if (shell.exec('git commit -am "chore: update version"').code !== 0) {
+  //     shell.echo('\nError committing changes. Exiting.');
+  //     throw new Error('Error committing changes');
+  //   }
+  // } catch (e) {
+  //   shell.echo(e);
+  //   try {
+  //     // try to revert changes if there was an error
+  //     fs.writeFileSync('package.json', `${JSON.stringify(packageJson, null, 2)}\n`);
+  //     shell.echo('\nPackage.json version reverted');
+  //     fs.writeFileSync('./src/release.md', releaseNotes);
+  //     shell.echo('\nRelease notes version reverted');
+  //     shell.exit(1);
+  //   } catch (error) {
+  //     // if there was an error reverting changes, exit
+  //     shell.echo(`\nError reverting package.json and release notes changes: ${error}`);
+  //     shell.echo(error);
+  //     shell.exit(1);
+  //   }
+  // }
+}
+try {
+  run();
 } catch (error) {
   core.setFailed(error.message);
 }
